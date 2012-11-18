9f2c1fc80e513eac6dd2b8d485a3ade352dd5f06
Stephen Repper <s.repper@yahoo.com>
2012-11-17 23:07:02 -0500

    Database.java small update
    Just some slight corrections/additions.  Falling asleep.

--- abbasi/dbtSQL/src/Database.java
+++ abbasi/dbtSQL/src/Database.java
@@ -3,27 +3,35 @@ import java.util.*;
 
 public class Database {
 	String name;
-	//Table table;
 	ArrayList<Table> tableList;
 	
 	public Database(ArrayList<String> args){
-		name = args.get(3);
+		name = args.get(2).replace(";", "").trim();	
 	}
 	
+	// drop the current database
 	public void dropDB(String filename){
+		//remove external database file
 		File rmFile = new File(filename);
 		rmFile.delete();
+		
+		name = null;
 	}
 	
 	
-	
+	// create a new table in the database
 	public void createTable(ArrayList<String> args){
 		tableList.add(new Table(args));
 	}
+	
 	
 	public boolean dbCheck(String dbName){
 		if(dbName == name)
 			return true;
 		return false;
+	}
+	
+	public String getName(){
+		return name;
 	}
 }
