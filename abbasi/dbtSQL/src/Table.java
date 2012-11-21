9c2c170c26cb7116c620967e7b464d3ede677f5b
Stephen Repper <s.repper@yahoo.com>
2012-11-21 16:46:28 -0500

    Table.java
    Cleaned up code, added comments.

--- abbasi/dbtSQL/src/Table.java
+++ abbasi/dbtSQL/src/Table.java
@@ -5,6 +5,7 @@ public class Table {
 	ArrayList<ArrayList<Object>> rows;
 	
 	public Table(String command){
+		// pull table name, trim whitespace before opening parenthesis
 		name = command.substring(0, command.indexOf('('));
 		command = command.substring(name.length(), command.length()).trim();
 		ArrayList<String> cmdList = createCmd(command);
@@ -15,25 +16,39 @@ public class Table {
 		ArrayList<String> parsedStatement = new ArrayList<String>();
 		String cmd = command;
 		
-		cmd = cmd.replace(";", "");
+		// remove semicolon & trim white space
+		cmd = cmd.replace(";", "").trim();
+		
+		// check for opening & closing parentheses
 		if(!(cmd.charAt(0) == '(' && cmd.charAt(cmd.length()-1) == ')')){
 			System.out.println("Syntax error in Create Table command");
 			return null;
 		}
+		
+		// remove leading parenthesis
 		cmd = cmd.substring(1, cmd.length());
 		
 		String temp;
 		int pos = 0;
+		
+		// parse data types & labels
 		while(pos < cmd.length()){
-			if(cmd.substring(pos, cmd.lastIndexOf(')')).contains(",")){
+			
+			// if comma exists, pull string before comma index
+			if(cmd.substring(pos, cmd.lastIndexOf(')')).contains(","))
 				temp = cmd.substring(pos, cmd.indexOf(','));
-				//pos = pos + temp.length()+1;
-			}
+			// else pull rest of string (last term)
 			else
 				temp = cmd.substring(pos, cmd.lastIndexOf(')'));
+			
+			// increase pos to index after comma
 			pos = pos + temp.length()+1;
+			
+			// if string contains unmatched parenthesis
 			if(temp.contains("(") && !temp.contains(")")){
-				temp = temp + ",";
+				temp = temp + ","; //insert skipped comma
+				
+				// if another comma exists, append substring
 				if(cmd.substring(pos, cmd.length()).contains(",")){
 					int i = pos;
 					while(cmd.charAt(i) != ',')
@@ -41,33 +56,19 @@ public class Table {
 					temp = temp + cmd.substring(pos, i);
 					pos = i + 1;
 				}
-				else
-					temp = temp + cmd.substring(pos, cmd.lastIndexOf(')'));	
+				// else (no comma) pull rest of string (final term)
+				else{
+					temp = temp + cmd.substring(pos, cmd.lastIndexOf(')'));
+					pos = cmd.lastIndexOf(')');
+				}
 			}
+			
+			// trim temp string, set new trimmed cmd string, reset pos to 0
+			temp = temp.trim();
 			cmd = cmd.substring(pos, cmd.length()).trim();
 			pos = 0;
 				
-		}
-		//while(i < cmd.length()){
-			
-		//}
-		
-		//StringTokenizer st = new StringTokenizer(cmd, ",");
-		
-		//if(cmd.toUpperCase().contains("VALUES")){
-		//	;
-		//}
-		
-		//while(st.hasMoreTokens()){
-		//	String temp = st.nextToken();
-		//	if(temp.charAt(0) == '(')
-		//		temp.replaceFirst("(", "");
-			
-		//	
-		//	parsedStatement.add(temp);	
-		//}
-		
-		
+		}	
 		
 		return parsedStatement;
 	}
