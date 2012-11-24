227474f824b0c862b6bd21df4922e92c40db3aac
nihar goswami <nihargoswami@gmail.com>
2012-11-24 14:22:16 -0500

    adding changes

--- abbasi/batch file/DBT.bat
+++ abbasi/batch file/DBT.bat
@@ -1,3 +1,4 @@
 set $PATH="C:\Program Files\Java\jdk1.7.0_02\bin" //where the java lib
-cd C:\Users\GCN\Documents\GitHub\DBT-\abbasi\dbtSQL\bin  //needs to change where the file is
+cd C:\Users\GCN\Documents\GitHub\DBT-\abbasi\dbtSQL\bin
+
 java dbt
