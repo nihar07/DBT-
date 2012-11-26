fe44910563e7056c81c5fb6cce4981a05fb5485e
Stephen Repper <s.repper@yahoo.com>
2012-11-26 13:42:33 -0500

    I lied
    Had a typo.  Also changed .../jdk1.7.0_23/... to .../jdk*/... so that it
    works across multiple versions of java (I have jdk1.6.0_23.)  Test it
    out, make sure it works across different systems.

--- abbasi/batch file/DBT-Stephen.bat
+++ abbasi/batch file/DBT-Stephen.bat
@@ -1,3 +1,3 @@
-set $PATH="C:\Program Files\Java\jdk1.6.0_23\bin" 
+set $PATH="C:\Program Files\Java\jdk*\bin" 
 cd ..\dbtSQL\bin
 java -cp .;./xstream-1.4.3.jar;./xpp3-1.1.4c.jar;./xmlpull_1_0_5.jar dbt
--- abbasi/batch file/DBT.bat
+++ abbasi/batch file/DBT.bat
@@ -1,4 +1,4 @@
-set $PATH="C:\Program Files\Java\jdk1.7.0_02\bin" 
+set $PATH="C:\Program Files\Java\jdk*\bin" 
 cd ..\dbtSQL\bin  
 
-java -cp .;./xstream-1.4.3.jar;./xpp3-1.1.4c.jar;./xmlpull_1_0_5.jar dbtdbt
+java -cp .;./xstream-1.4.3.jar;./xpp3-1.1.4c.jar;./xmlpull_1_0_5.jar dbt
